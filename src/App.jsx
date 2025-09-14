ff74f56640d2a7a99f0ac6309e981bf40d7e5f5c
shahadot99999 <shahadot99999@gmail.com>
2025-09-14 19:33:21 +0600

    Fetch for Post method and Send Client data to the server

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,28 @@ function App() {
     .then(res=>res.json())
     .then(data =>setUsers(data));
   },[])
+
+  const handleAddUser = e =>{
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const user = {name, email}
+    console.log(user);
+
+    //create user in the server
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+
+    })
+    .then(res => res.json())
+    .then(data=>{
+      console.log('data after post', data);
+    })
+  }
   return (
     <>
       
@@ -18,9 +40,9 @@ function App() {
       <h3>Numbers of Users: {users.length}</h3>
 
       <form onSubmit={handleAddUser} >
-        <input type="text" name='' id='' />
+        <input  type="text" name='name' id='' />
         <br />
-        <input type="email" name="" id="" />
+        <input type="email" name="email" id="" />
         <br />
         <input type="submit" value="add user" />
       </form>
