c4bf51d4846070a70410a9e8455224f214e289fc
shahadot99999 <shahadot99999@gmail.com>
2025-09-14 18:56:12 +0600

    data loded in database

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    
+  const [users, setUsers]=useState([]);
 
+  useEffect(()=>{
+    fetch('http://localhost:5000/users')
+    .then(res=>res.json())
+    .then(data =>setUsers(data));
+  },[])
   return (
     <>
+      
+      <h1>Users Managements System</h1>
+      <h3>Numbers of Users: {users.length}</h3>
+
+      <form onSubmit={handleAddUser} >
+        <input type="text" name='' id='' />
+        <br />
+        <input type="email" name="" id="" />
+        <br />
+        <input type="submit" value="add user" />
+      </form>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {
+          users.map(user => <p key={user.id}>{user.id} :  {user.name} : {user.email}</p>)
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
