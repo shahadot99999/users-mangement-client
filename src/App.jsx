b81970400ffa1fe9d0fadd41a2746395cace9f89
shahadot99999 <shahadot99999@gmail.com>
2025-09-15 16:17:34 +0600

    Display data on the client

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,9 @@ function App() {
     .then(res => res.json())
     .then(data=>{
       console.log('data after post', data);
+      const newUsers = [...users, data];
+      setUsers(newUsers);
+      e.target.reset();
     })
   }
   return (
